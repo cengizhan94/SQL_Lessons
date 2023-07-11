8152cae1d19d267d6203d326aa6be4b5efa3eaef
Cengiz Han <sonsair11@gmail.com>
2023-07-11 13:01:52 +0300

    using django admin interface

--- airlane/flights/views.py
+++ airlane/flights/views.py
@@ -3,4 +3,8 @@ from .models import Flight
 # Create your views here.
 def index(request):
     return render(request, 'flights/index.html', {"flights": Flight.objects.all()})
+
+def flight(request,flight_id):
+    flight = flight.objects.get(pk=flight_id)
+    return render(request, 'flights/flight.html', {"flight": flight})
         
