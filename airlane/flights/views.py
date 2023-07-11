072f3299518345e5d073d543fcde33896e9ef83f
Cengiz Han <sonsair11@gmail.com>
2023-07-11 11:59:03 +0300

    Fixed syntax errors in code

--- airlane/flights/views.py
+++ airlane/flights/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from . import Flight
+from .models import Flight
 # Create your views here.
 def index(request):
-    return render(request, "flights/index.html",{
-        "flights": Flight.objects.all()
-    })
+    return render(request, 'flights/index.html', {"flights": Flight.objects.all()})
         
